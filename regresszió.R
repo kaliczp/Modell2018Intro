785f50fe6c240fa721d4b952caeeb5364266e5f5
Péter Kalicz <kaliczp@gmail.com>
2018-04-06 12:28:22 +0200

    hist color

--- regresszió.R
+++ regresszió.R
@@ -1,1 +1,10 @@
-x = rnorm(20000)
+x = rnorm(20000, mean=4)
+x = rnorm(20000, 4) # ugyan az, mint a fenti, a sorrend!
+
+## EDA x-re
+hist(x, col=2)
+# http://colorbrewer2.org #hexa kód
+hist(x, col="#d7301f")
+hist(x, col="red")
+
+y = 2*x
