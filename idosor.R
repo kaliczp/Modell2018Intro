f7edf2a02631e1b144d0caa6ced5bb07238be4f5
Péter Kalicz <kaliczp@gmail.com>
2018-04-27 13:23:01 +0200

    higher degree polynoms

--- idosor.R
+++ idosor.R
@@ -11,7 +11,30 @@ plot(temp.df)
 lines(temp.lowess)
 temp.lowess5 <- lowess(temp.df,f=1/28)
 lines(temp.lowess5, col=2)
+
+plot(temp.df$ido, temp.df$t-temp.lowess5$y)
+lines(lowess(temp.df$ido, temp.df$t-temp.lowess5$y), col=2)
+
+
 ## Regresszió
 temp.reg <- lm(t ~ ido, temp.df)
 abline(temp.reg,col=3)
 plot(temp.reg)
+## Regresszió diagnosztika
+plot(resid(temp.reg) ~ temp.df$ido)
+lines(lowess(temp.df$ido, resid(temp.reg)),col=2)
+
+## Másodfokú polinom
+temp.reg2 <- lm(t ~ ido + I(ido^2), temp.df)
+summary(temp.reg2)
+lines(fitted(temp.reg2) ~ temp.df$ido,col=4)
+## Negyedfokú polinom
+temp.reg4 <- lm(t ~ poly(ido,4), temp.df)
+lines(fitted(temp.reg4) ~ temp.df$ido,col=5)
+
+## Előrejelzés
+plot(temp.df, xlim=c(1880,2050),ylim=c(-0.5,6))
+
+lines(2018:2050,predict(temp.reg,data.frame(ido=2018:2050)),col=1)
+lines(2018:2050,predict(temp.reg2,data.frame(ido=2018:2050)),col=2)
+lines(2018:2050,predict(temp.reg4,data.frame(ido=2018:2050)),col=3)
