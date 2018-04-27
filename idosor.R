91d2931470bfb462c67117b25b97985362a4ad11
Péter Kalicz <kaliczp@gmail.com>
2018-04-27 12:44:06 +0200

    loess smoother

--- idosor.R
+++ idosor.R
@@ -4,3 +4,7 @@ teljes[1,14]
 éves <- as.numeric(as.character(teljes[, "J.D"]))
 plot(éves)
 temp.df <- data.frame(ido = 1880:2018,t = éves)
+temp.df <- temp.df[-nrow(temp.df),]
+# Nem-paraméteres módszer lokális regresszió
+temp.lowess <- lowess(temp.df)
+lines(temp.lowess)
