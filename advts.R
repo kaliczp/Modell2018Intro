1196249d15f7795952685003f9df848fa2c2767d
Péter Kalicz <kaliczp@gmail.com>
2018-05-04 11:29:27 +0200

    co2 data

--- advts.R
+++ advts.R
@@ -14,3 +14,18 @@ lines(t.tsf9,col=3,lwd=2)
 t.tsf <- filter(t.ts,c(1/8,rep(1/4,3),1/8))
 lines(t.tsf,col=4,lwd=2)
 
+## CO2
+?co2
+co2
+tsp(co2)
+end(co2)
+time(co2)
+time(co2)[1:13] #Az idő paraméter az első évben
+cycle(co2)
+as.vector(co2)[1:10] #Koncentráció vektora
+
+plot(co2)
+
+plot(co2,main="Szén-dioxid koncentráció (1959-1997)",xlab="",ylab="[ppm]",xaxs="i",typ="n")
+grid(col="grey")
+lines(co2,lwd=2)
