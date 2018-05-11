afd759718b3d1343c1ce8b2885aee2eaf9c4d53f
Péter Kalicz <kaliczp@gmail.com>
2018-05-11 11:24:39 +0200

    Some statistical simulation

---
+++ jatek.R
@@ -0,0 +1,10 @@
+ttnum=100
+x=runif(ttnum)
+y=runif(ttnum)
+plot(x,y,asp=1,pch=".",xaxs="i", yaxs="i")
+lines(seq(0,1,0.01),sqrt(1-(seq(0,1,0.01)^2)))
+dist=sqrt(x^2+y^2)
+inside=ifelse(dist <= 1,TRUE,FALSE)
+points(x[inside],y[inside],col=2,pch=".")
+#inside=ifelse(dist <= 1,1,0)
+4*(sum(inside)/ttnum)
