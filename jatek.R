55531fb4f71f65d1157e68db39c1be1c14d19474
Péter Kalicz <kaliczp@gmail.com>
2018-05-11 12:35:02 +0200

    enhanced function

--- jatek.R
+++ jatek.R
@@ -1,11 +1,14 @@
-pi.gen <- function(ttnum=1000000)
+pi.gen <- function(ttnum, plot=FALSE)
 {
   x=runif(ttnum)
   y=runif(ttnum)
-  plot(x,y,asp=1,pch=".",xaxs="i", yaxs="i")
-  lines(seq(0,1,0.01),sqrt(1-(seq(0,1,0.01)^2)))
   dist=sqrt(x^2+y^2)
   inside=ifelse(dist <= 1,TRUE,FALSE)
-  points(x[inside],y[inside],col=2,pch=".")
+  if(plot)
+  {
+    plot(x,y,asp=1,pch=".",xaxs="i", yaxs="i")
+    lines(seq(0,1,0.01),sqrt(1-(seq(0,1,0.01)^2)))
+    points(x[inside],y[inside],col=2,pch=".")
+  }
   4*(sum(inside)/ttnum)
 }
